ca4c81ffcac22801495a19fa0fb6491d702abe27
ktokto313 <kto3132006@gmail.com>
2026-06-01 15:22:27 +0700

    Update according to comment

--- Backend/src/main/java/swp391/group6/dto/LoginResponse.java
+++ Backend/src/main/java/swp391/group6/dto/LoginResponse.java
@@ -3,14 +3,12 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class LoginResponse {
-    private String token;
     private String email;
     private String fullName;
     private String role;
     private String error;
 
-    public LoginResponse(String token, String email, String fullName, String role) {
-        this.token = token;
+    public LoginResponse(String email, String fullName, String role) {
         this.email = email;
         this.fullName = fullName;
         this.role = role;
@@ -20,7 +18,6 @@ public class LoginResponse {
         this.error = error;
     }
 
-    public String getToken() { return token; }
     public String getEmail() { return email; }
     public String getFullName() { return fullName; }
     public String getRole() { return role; }
