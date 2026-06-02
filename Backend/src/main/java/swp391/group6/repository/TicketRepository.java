dbfa5b35c977db05f70c26dc80d2e105a3775bb5
PlotChat <levietanh1234567@gmail.com>
2026-06-02 10:22:49 +0700

    feat: add a new find method in ticket repository
    - This new method will return a list of tickets by the creator or assignee

--- Backend/src/main/java/swp391/group6/repository/TicketRepository.java
+++ Backend/src/main/java/swp391/group6/repository/TicketRepository.java
@@ -1,6 +1,8 @@
 package swp391.group6.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import swp391.group6.model.Ticket;
 import swp391.group6.model.TicketState;
 
@@ -9,4 +11,7 @@ import java.util.List;
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
     List<Ticket> findByTicketCreator(long id);
     List<Ticket> findByTicketStateNot(TicketState state);
+
+    @Query("SELECT t FROM Ticket t WHERE t.ticketCreator.id = :userId OR t.assignee.id = :userId")
+    List<Ticket> findTicketsByCreatorOrAssignee(@Param("userId") long userId);
 }
